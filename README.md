f33d8adc0b08d57de8911b60121bb16036da6834
gavjager <gavjager@umich.edu>
2020-12-15 16:34:09 -0500

    added commentary for winter-answer

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ German 472 Project. Linked dynamic translation website.
 ## TO DO:
 - Format Biography, improve text, maybe add a photo
 - Find/add bio references
-- Add commentary for Winter-answer
 - Add commentary Part of the Question
 - Add Auf Sicht
 - Add Außer Landes?
--- js/winter-answer.js
+++ js/winter-answer.js
@@ -26,31 +26,75 @@ var german_text =
 var gen_commentary = 
 '<h3>Translating "Winter-Answer"</h3>' +
 "<p>" +
+"Winter-answer is a poem of the senses and perception. The title is another example of " +
+"a German compound, one which in this case does not lend itself to an obvious interpretation " +
+"even with the context of the poem. It is clear that the poem is advocating for a return " +
+"to some other mode of perception, one which even the grandmother has forgotten. " +
+"Perhaps it can be seen in the light of the sense-dullness and isolation of winter, in which " +
+'the sky is darker for longer, the birds have migrated, and there is no "grass to taste." ' +
+'With this in mind, "Winterantwort" can be seen as an answer to the numbness of winter, ' +
+'even as this "answer" is being sought out in the "finsterer Wald" – the dark, presumably wintry woods. ' + 
+'Here we find a thematic parallel to the "bleak/bright" dynamic present in <a href="winter-early.html">"Winter-early."</a> ' +
+'The symbolism of the "Wald" is very important. Usually translated as forest, I decided to go with "woods" throughout. '  +
+'It can be seen as representing the unknown; it has dark and mysterious properties, and is ' +
+'not what it initially seems to be in the poem. I believe "woods" possesses some of the same mysterious connotations, ' +
+'unlike "forest," which seems more geographical. This idea of venturing out into the unknown, ' +
+'following the children who have grown up on the edge of some new mode of perception, some new paradigm or era – ' +
+'this idea is central to many of the poems in this collection. ' +
+'Aichinger advocates for societal change throughout, which is not surprising given her involvment in ' +
+'<a href="aichinger-v_r.html">Gruppe 47.</a>' +
 "</p>";
 
 var weltstoff =
-'<h3>Commentary: Considering the fabric of the world</h3>' +
+'<h3>Considering the fabric of the world</h3>' +
 "<p>" +
+'Because of the structure of the first two lines in German, as well as the differences in mode of possible expression compared ' +
+'to English, the beginning of this poem was very difficult to translate. The word "Stoff," while it has a clear ' +
+'etymylogical link to "stuff" in English, has a wider breadth of meaning and usage which does not match up with the ' +
+'usually informal connotation of "stuff." I did, however, use it in two of the alternate translations, hoping to evoke an ' +
+'echo of Shakespeare\'s: "we are such stuff as dreams are made of," which closely mirrors the usage here. ' +
+'However, outside of a dynamic translation, I would have likely chosen "fabric" or "nature of the world." ' +
+'The word "Betrachtung" in the German, which also has a wide breadth of meaning, encapsulating "contemplation," ' +
+'"consideration," "discourse," and elements of "examination," was another obvious choice for a dynamic element in the poem. ' +
+'With the multiple variants here, I tried to convey a sense of both the importance of perception, and the ' +
+'idea of "mulling things over."' +
 "</p>";
 
 var augen_und_ohren =
-'<h3>Commentary: Eyes and Ears</h3>' +
+'<h3>Eyes and Ears</h3>' +
 "<p>" +
+'The sense that the original poem conveys here is one of purpose. It is clear of the eyes and ears mentioned ' +
+'that they were "eyes in order to see," and "ears in order to hear," not just that they could potentially be used ' +
+'to look at fields and hear birds. I tried to give the translation that same sense by emphasizing the "lack" ' +
+'of each, and using the construction "eyes with which to see," instead of simply "eyes to see." ' +
 "</p>";
 
 var keine_augen_mehr = 
-'<h3>Commentary: Physical and Temporal</h3>' +
+'<h3>Physical and Temporal</h3>' +
 "<p>" +
+'I thought this would be an interesting place to show this type of translation\'s potential ' +
+'for emphasizing different aspects of meaning. The original German corresponding to this element ' +
+'conveys the sense of both a personal loss of the senses and a general societal loss of the senses over time.' +
+'While the English "no more eyes," probably does this sufficiently well, I thought it would be interesting to ' +
+'include both variants to highlight this distinction.' +
 "</p>";
 
 var schmecken = 
-'<h3>Commentary: A German Verb</h3>' +
+'<h3>A German Verb</h3>' +
 "<p>" +
+'The German verb "schmecken" – to taste, is a very interesting one, in that it has positive connotations as a default.' +
+'When something tastes good in German, the phrase is "das schmeckt mir" – that tastes to me. And while the appeal of ' +
+'eating grasses is somewhat limited in the general, non-bovine population, there is a positive association built into ' +
+'the language of the original poem.' +
 "</p>";
 
 var himmel_zu_ende = 
-'<h3>Commentary: An Intricate Phrase</h3>' +
+'<h3>An Intricate Phrase</h3>' +
 "<p>" +
+'On first reading, I was not certain I had understood this correctly. The line is "und wer riecht uns den Himmel zu Ende," ' +
+'literally: "and who smells us the sky to end." The German grammar helps this to make more sense, but ' + 
+'finding an English representation for the weirdness and completeness and potential freedom contained here was no easy task. ' +
+'I tried to pick variants which represented these ideas in their structure and wording.' +
 "</p>";
 
 var commentaries = [
